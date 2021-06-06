2aa610f3b81c2001f4b4b41a9384c4c4cf1eb242
Ubuntu <ubuntu@ip-172-31-44-60.ap-northeast-2.compute.internal>
2021-06-06 17:38:26 +0900

    배포시 mysql 의 인코딩 문제 때문에 한글이 제대로 insert 안되던 문제 해결

--- db.sql
+++ db.sql
@@ -5,4 +5,5 @@ create database diary default character set utf8;
 create user seung@'%' identified by '12341234';
 grant all privileges on diary.* to seung@'%';
 alter user seung@'%' identified with mysql_native_password by '12341234';
+alter database diary default character set utf8;
 
